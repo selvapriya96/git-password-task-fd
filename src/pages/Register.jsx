d9a56adefc42d673badc6c0e5e8df26c315eb6d6
selvapriya96 <rsselvapriya96@gmail.com>
2025-12-01 19:17:48 +0530

    resolve error in register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,20 +4,17 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-  const [form, setForm] = useState({ name: "", email: "", password: "" });
+  const [data, setData] = useState({ name: "", email: "", password: "" });
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await api.post("/register", form); 
+      const res = await api.post("/register", data);
       toast.success(res.data.message || "Registered successfully");
 
-      setForm({ name: "", email: "", password: "" });
-
-   
+      setData({ name: "", email: "", password: "" });
       setTimeout(() => navigate("/login"), 1500);
-
     } catch (err) {
       toast.error(err.response?.data?.message || "Registration failed");
     }
@@ -28,36 +25,32 @@ export default function Register() {
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
-
           <input
             name="name"
-            value={form.name}
-            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            value={data.name}
+            onChange={(e) => setData({ ...data, name: e.target.value })}
             placeholder="Name"
             className="w-full p-2 border rounded"
             required
           />
-
           <input
             name="email"
-            value={form.email}
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
             type="email"
             placeholder="Email"
             className="w-full p-2 border rounded"
             required
           />
-
           <input
             name="password"
-            value={form.password}
-            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
             type="password"
             placeholder="Password"
             className="w-full p-2 border rounded"
             required
           />
-
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded"
